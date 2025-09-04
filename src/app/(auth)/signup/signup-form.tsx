dedb99397e57520b4a469ce1c7126045a2cbc672
Minh Nguyen <anhminh.dev@gmail.com>
2025-09-04 09:58:33 +0700

    refactor: remove birthday field from signup form; update validation schema and auth service accordingly

--- src/app/(auth)/signup/signup-form.tsx
+++ src/app/(auth)/signup/signup-form.tsx
@@ -2,13 +2,11 @@
 'use client';
 
 import { z } from 'zod';
-import { format } from 'date-fns';
 import { useForm } from 'react-hook-form';
 import { useRouter } from 'next/navigation';
-import { AtSign, CalendarIcon, CircleUser, Lock } from 'lucide-react';
+import { AtSign, CircleUser, Lock } from 'lucide-react';
 import { IoMale, IoFemale } from 'react-icons/io5';
 import { zodResolver } from '@hookform/resolvers/zod';
-
 import {
   Form,
   FormControl,
@@ -17,16 +15,9 @@ import {
   FormLabel,
   FormMessage,
 } from '@/components/ui/form';
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@/components/ui/popover';
-import { cn } from '@/lib/utils';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { signupSchema } from '@/lib/validation';
-import { Calendar } from '@/components/ui/calendar';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { signup } from '@/services/auth';
 import toast from 'react-hot-toast';
@@ -40,7 +31,6 @@ const SignupForm = () => {
       email: '',
       password: '',
       username: '',
-      birthday: new Date().toISOString(),
       gender: 'male',
     },
   });
@@ -117,47 +107,6 @@ const SignupForm = () => {
             )}
           />
           <div className="grid grid-cols-2 gap-5">
-            <FormField
-              control={form.control}
-              name="birthday"
-              render={({ field }) => (
-                <FormItem className="w-full">
-                  <FormLabel>Birthday</FormLabel>
-                  <FormControl>
-                    <Popover>
-                      <PopoverTrigger asChild>
-                        <Button
-                          variant="outline"
-                          className={cn(
-                            'w-full justify-start text-left font-normal',
-                            !field.value && 'text-muted-foreground',
-                          )}
-                        >
-                          <CalendarIcon className="mr-3 h-4 w-4" />
-                          {field.value ? (
-                            format(field.value, 'PPP')
-                          ) : (
-                            <span>Pick a date</span>
-                          )}
-                        </Button>
-                      </PopoverTrigger>
-                      <PopoverContent align="start" className="w-auto p-0">
-                        <Calendar
-                          mode="single"
-                          captionLayout="dropdown-buttons"
-                          selected={
-                            field.value ? new Date(field.value) : undefined
-                          }
-                          onSelect={field.onChange}
-                          fromYear={1960}
-                          toYear={2030}
-                        />
-                      </PopoverContent>
-                    </Popover>
-                  </FormControl>
-                </FormItem>
-              )}
-            />
             <FormField
               control={form.control}
               name="gender"
--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,39 +1,38 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const signupSchema = z.object({
-  email: z.string().email({ message: "Invalid email address" }).trim(),
+  email: z.string().email({ message: 'Invalid email address' }).trim(),
   password: z
     .string()
-    .min(8, { message: "Be at least 8 characters long" })
+    .min(8, { message: 'Be at least 8 characters long' })
     .regex(/[A-Z]/, {
-      message: "Contain at least one uppercase letter.",
+      message: 'Contain at least one uppercase letter.',
     })
     .regex(/[a-z]/, {
-      message: "Contain at least one lowercase letter.",
+      message: 'Contain at least one lowercase letter.',
     })
     .regex(/[0-9]/, {
-      message: "Contain at least one number.",
+      message: 'Contain at least one number.',
     })
     .regex(/[^a-zA-Z0-9]/, {
-      message: "Contain at least one special character.",
+      message: 'Contain at least one special character.',
     })
     .trim(),
   username: z
     .string()
-    .min(3, { message: "Be at least 3 characters long." })
+    .min(3, { message: 'Be at least 3 characters long.' })
     .regex(/^[a-zA-Z0-9]+$/, {
-      message: "Only letters and numbers are allowed.",
+      message: 'Only letters and numbers are allowed.',
     })
     .trim(),
-  birthday: z.string(),
   gender: z.string(),
 });
 
 export const loginSchema = z.object({
-  email: z.string().email({ message: "Invalid email address" }),
+  email: z.string().email({ message: 'Invalid email address' }),
   password: z
     .string()
-    .min(6, { message: "Password must be at least 6 characters" }),
+    .min(6, { message: 'Password must be at least 6 characters' }),
 });
 
 export const createPostSchema = z.object({
